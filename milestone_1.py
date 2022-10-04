2dd1ebded9fe056a9e64a6e4a2c2a51f90919751
NazWaz <wazidur123@gmail.com>
2022-10-04 22:07:57 +0100

    cleans up code

--- milestone_1.py
+++ milestone_1.py
@@ -1,14 +1,14 @@
 # %%
 import random # imports random module
 word_list = ["apple", "banana", "pear", "orange", "grape"] # creates the list of fruits
-print (word_list) # prints the list of fruits
+print(word_list) # prints the list of fruits
 # %%
 word = random.choice(word_list) # chooses a word at random from the list
-print (word) # prints random word
+print(word) # prints random word
 # %%
 guess = input("Enter a single letter") # asks for user input
+print(guess) # prints user input
 if len(guess) == 1 and guess.isalpha(): # checks input is 1 character and from the alphabet
-    print ("Good guess!") 
+    print("Good guess!") 
 else: 
-    print ("Oops! That is not a valid input.")
-print (guess) # prints user input
+    print("Oops! That is not a valid input.")
