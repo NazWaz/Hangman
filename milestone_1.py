44c09f8b587bba579700c201789a1d81d412e727
NazWaz <wazidur123@gmail.com>
2022-10-04 21:47:54 +0100

    Asks the user for an input

--- milestone_1.py
+++ milestone_1.py
@@ -2,10 +2,10 @@
 import random
 word_list = ["apple", "banana", "pear", "orange", "grape"] # creates the list of fruits
 print (word_list)
-
-
 # %%
 word = random.choice(word_list) # chooses a word at random from the list
 print (word)
-
+# %%
+guess = input("Enter a single letter")
+print (guess)
 # %%
